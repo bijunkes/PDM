27bb2bedda5537f5fb034ba01c50010aa3aa66f0
bijunkes <bianca.jr2007@gmail.com>
2025-09-01 09:03:18 -0300

    Sorteado

--- app/src/main/java/com/example/aula2/MainActivity.java
+++ app/src/main/java/com/example/aula2/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.aula2;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,17 +12,34 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class  MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    EditText editTextMin, editTextMax;
+
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        button = findViewById(R.id.button);
+        editTextMin = findViewById(R.id.edMin);
+        editTextMax = findViewById(R.id.edMax);
+        tv = findViewById(R.id.tvResultado);
+
+        button.setOnClickListener(v -> {
+            Random random = new Random();
+            int min, max;
+            min = Integer.parseInt(editTextMin.getText().toString());
+            max = Integer.parseInt(editTextMax.getText().toString());
+            int delta = max - min;
+            int sorteado = random.nextInt(delta) + min;
+            tv.setText(Integer.toString(sorteado));
         });
     }
 
