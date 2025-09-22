2b8b45dd6128c45d1f31cf9545bbdcc93769d04a
bijunkes <bianca.jr2007@gmail.com>
2025-09-22 09:38:43 -0300

    imc finalizado

--- app/src/main/java/com/example/aula2/imcResultado.java
+++ app/src/main/java/com/example/aula2/imcResultado.java
@@ -19,7 +19,7 @@ public class imcResultado extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_resultado);
+        setContentView(R.layout.imc_resultado);
 
         textResultado = findViewById(R.id.textResultado);
         textPeso = findViewById(R.id.textResultadoPeso);
