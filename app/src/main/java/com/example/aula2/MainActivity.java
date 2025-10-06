bcf6bf77a7c486bf01f91446ddf141a95cdcecf1
bijunkes <bianca.jr2007@gmail.com>
2025-10-06 08:54:15 -0300

    Listagem com editText

--- app/src/main/java/com/example/aula2/MainActivity.java
+++ app/src/main/java/com/example/aula2/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.aula2;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +13,42 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    ListView listNome;
+    EditText textNome;
+    Button buttonSalvar;
+    ArrayList<String> nomes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        listNome = findViewById(R.id.listNome);
+        textNome = findViewById(R.id.textNome);
+        buttonSalvar = findViewById(R.id.buttonSalvar);
+        nomes= new ArrayList<>();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, nomes);
+        listNome.setAdapter(adapter);
+
+        buttonSalvar.setOnClickListener(e -> {
+            String nome = textNome.getText().toString();
+            if (!nome.isEmpty()){
+                nomes.add(nome);
+                adapter.notifyDataSetChanged();
+            }
         });
+
+        listNome.setOnItemLongClickListener((parent, view, position, id) -> {
+            nomes.remove(position);
+            adapter.notifyDataSetChanged();
+            return true;
+        });
+
     }
+
 }
