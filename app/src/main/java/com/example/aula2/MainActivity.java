8fe9c68a7b46b42b67b33ecd29226854dfb979f7
bijunkes <bianca.jr2007@gmail.com>
2025-10-20 09:01:32 -0300

    20/10

--- app/src/main/java/com/example/aula2/MainActivity.java
+++ app/src/main/java/com/example/aula2/MainActivity.java
@@ -28,9 +28,12 @@ public class MainActivity extends AppCompatActivity {
         listView = findViewById(R.id.listView);
         planetaController = new PlanetaController();
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+        PlanetaAdapter adapter = new PlanetaAdapter(
                 this,
-                android.R.layout.simple_list_item_1, planetaController.getNomePlanetas());
+                R.layout.item_lista,
+                planetaController.getPlaneta()
+        );
+
         listView.setAdapter(adapter);
 
     }
